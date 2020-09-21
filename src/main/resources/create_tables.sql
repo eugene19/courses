b6a71cfc688490cf5074bf007d0fce0934da9247
degtyaryov <Degtyarov96@mail.ru>
2020-09-21 07:40:32 +0300

    Add SQL scripts of creation and insertion tables

---
+++ src/main/resources/create_tables.sql
@@ -0,0 +1,106 @@
+CREATE TABLE IF NOT EXISTS `COURSES_MANAGER`.`user_roles`
+(
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `role` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `ID_UNIQUE` (`id` ASC) VISIBLE
+)
+    DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `COURSES_MANAGER`.`courses`
+(
+    `id`             INT          NOT NULL AUTO_INCREMENT,
+    `summary`        VARCHAR(255) NOT NULL,
+    `description`    LONGTEXT     NOT NULL,
+    `students_limit` INT          NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `ID_UNIQUE` (`id` ASC) VISIBLE
+)
+    DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `COURSES_MANAGER`.`users`
+(
+    `id`       INT          NOT NULL AUTO_INCREMENT,
+    `login`    VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `surname`  VARCHAR(45)  NOT NULL,
+    `name`     VARCHAR(45)  NOT NULL,
+    `email`    VARCHAR(255) NOT NULL,
+    `birthday` DATE         NOT NULL,
+    `role_id`  INT          NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `ID_UNIQUE` (`id` ASC) VISIBLE,
+    INDEX `ROLE_ID_idx` (`role_id` ASC) VISIBLE,
+    CONSTRAINT `ROLE_ID`
+        FOREIGN KEY (`role_id`)
+            REFERENCES `COURSES_MANAGER`.`user_roles` (`id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
+)
+    DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `COURSES_MANAGER`.`course_statuses`
+(
+    `id`     INT          NOT NULL AUTO_INCREMENT,
+    `status` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `ID_UNIQUE` (`id` ASC) VISIBLE
+)
+    DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `COURSES_MANAGER`.`course_results`
+(
+    `id`      INT          NOT NULL AUTO_INCREMENT,
+    `mark`    TINYINT      NOT NULL,
+    `comment` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `ID_UNIQUE` (`id` ASC) VISIBLE
+)
+    DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `COURSES_MANAGER`.`course_runs`
+(
+    `id`          INT  NOT NULL AUTO_INCREMENT,
+    `course_id`   INT  NOT NULL,
+    `start_date`  DATE NULL,
+    `end_date`    DATE NULL,
+    `lecturer_id` INT  NULL,
+    `status_id`   INT  NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `ID_UNIQUE` (`id` ASC) VISIBLE,
+    UNIQUE INDEX `COURSE_ID_UNIQUE` (`course_id` ASC) VISIBLE,
+    INDEX `LECTURER_ID_idx` (`lecturer_id` ASC) VISIBLE,
+    INDEX `STATUS_ID_idx` (`status_id` ASC) VISIBLE,
+    CONSTRAINT `COURSE_ID`
+        FOREIGN KEY (`course_id`)
+            REFERENCES `COURSES_MANAGER`.`courses` (`id`),
+    CONSTRAINT `LECTURER_ID`
+        FOREIGN KEY (`lecturer_id`)
+            REFERENCES `COURSES_MANAGER`.`users` (`id`),
+    CONSTRAINT `STATUS_ID`
+        FOREIGN KEY (`status_id`)
+            REFERENCES `COURSES_MANAGER`.`course_statuses` (`id`)
+)
+    DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `COURSES_MANAGER`.`user_courses`
+(
+    `id`               INT NOT NULL AUTO_INCREMENT,
+    `user_id`          INT NOT NULL,
+    `course_id`        INT NOT NULL,
+    `course_result_id` INT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `ID_UNIQUE` (`id` ASC) VISIBLE,
+    INDEX `USER_idx` (`user_id` ASC) VISIBLE,
+    INDEX `COURSE_idx` (`course_id` ASC) VISIBLE,
+    CONSTRAINT `USER`
+        FOREIGN KEY (`user_id`)
+            REFERENCES `COURSES_MANAGER`.`users` (`id`),
+    CONSTRAINT `COURSE`
+        FOREIGN KEY (`course_id`)
+            REFERENCES `COURSES_MANAGER`.`courses` (`id`),
+    CONSTRAINT `COURSE_RESULT_ID`
+        FOREIGN KEY (`course_result_id`)
+            REFERENCES `COURSES_MANAGER`.`course_results` (`id`)
+)
+    DEFAULT CHARACTER SET = utf8;
---
+++ src/main/resources/insert_test_date.sql
@@ -0,0 +1,63 @@
+INSERT INTO user_roles (id, role)
+VALUES (1, 'STUDENT');
+INSERT INTO user_roles (id, role)
+VALUES (2, 'LECTURE');
+
+INSERT INTO course_statuses (id, status)
+VALUES (1, 'NOT_STARTED');
+INSERT INTO course_statuses (id, status)
+VALUES (2, 'IN_PROGRESS');
+INSERT INTO course_statuses (id, status)
+VALUES (3, 'FINISHED');
+
+INSERT INTO users (id, login, password, surname, name, email, birthday, role_id)
+VALUES (1, 'user1', 111, 'Surname1', 'Name1', 'email1@test.ru', '2018-08-15',
+        1);
+INSERT INTO users (id, login, password, surname, name, email, birthday, role_id)
+VALUES (2, 'user2', 111, 'Surname2', 'Name2', 'email2@test.ru', '2018-08-15',
+        2);
+INSERT INTO users (id, login, password, surname, name, email, birthday, role_id)
+VALUES (3, 'user3', 111, 'Surname3', 'Name3', 'email3@test.ru', '2018-08-15',
+        2);
+
+INSERT INTO courses (id, summary, description, students_limit)
+VALUES (1, 'Summary1', 'Description1', 10);
+INSERT INTO courses (id, summary, description, students_limit)
+VALUES (2, 'Summary2', 'Description2', 10);
+INSERT INTO courses (id, summary, description, students_limit)
+VALUES (3, 'Summary3', 'Description3', 10);
+INSERT INTO courses (id, summary, description, students_limit)
+VALUES (4, 'Summary4', 'Description4', 10);
+INSERT INTO courses (id, summary, description, students_limit)
+VALUES (5, 'Summary5', 'Description5', 10);
+INSERT INTO courses (id, summary, description, students_limit)
+VALUES (6, 'Summary6', 'Description6', 10);
+
+INSERT INTO course_runs (id, course_id, start_date, end_date, lecturer_id,
+                         status_id)
+VALUES (1, 1, '2020-12-20', '2021-12-20', 2, 1);
+INSERT INTO course_runs (id, course_id, start_date, end_date, lecturer_id,
+                         status_id)
+VALUES (2, 2, '2020-12-20', '2021-12-20', 2, 2);
+INSERT INTO course_runs (id, course_id, start_date, end_date, lecturer_id,
+                         status_id)
+VALUES (3, 3, '2020-12-20', '2021-12-20', 2, 3);
+INSERT INTO course_runs (id, course_id, start_date, end_date, lecturer_id,
+                         status_id)
+VALUES (4, 4, '2020-12-20', '2021-12-20', 3, 1);
+INSERT INTO course_runs (id, course_id, start_date, end_date, lecturer_id,
+                         status_id)
+VALUES (5, 5, '2020-12-20', '2021-12-20', 3, 2);
+INSERT INTO course_runs (id, course_id, start_date, end_date, lecturer_id,
+                         status_id)
+VALUES (6, 6, '2020-12-20', '2021-12-20', 3, 3);
+
+INSERT INTO course_results (id, mark, comment)
+VALUES (1, 5, 'Good');
+INSERT INTO course_results (id, mark, comment)
+VALUES (2, 2, 'Bad student');
+
+INSERT INTO user_courses (id, user_id, course_id, course_result_id)
+VALUES (1, 1, 3, 1);
+INSERT INTO user_courses (id, user_id, course_id, course_result_id)
+VALUES (2, 1, 6, 2);
