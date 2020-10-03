22cfecc8ed602b9fdee3c5b1004c88d22c05cd64
degtyaryov <Degtyarov96@mail.ru>
2020-10-03 13:36:55 +0300

    Delete name of user role

--- src/main/java/by/epamtc/courses/entity/UserRole.java
+++ src/main/java/by/epamtc/courses/entity/UserRole.java
@@ -1,25 +1,13 @@
 package by.epamtc.courses.entity;
 
 public enum UserRole {
-    STUDENT(1, "Student"),
-    LECTURER(2, "Lecture");
+    STUDENT(1),
+    LECTURER(2);
 
     private int id;
-    private String text;
 
-    UserRole(int id, String text) {
+    UserRole(int id) {
         this.id = id;
-        this.text = text;
-    }
-
-    public static UserRole getById(int id) {
-        for (UserRole value : UserRole.values()) {
-            if (value.id == id) {
-                return value;
-            }
-        }
-
-        return null;
     }
 
     public int getId() {
@@ -28,13 +16,5 @@ public enum UserRole {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
     }
 }
