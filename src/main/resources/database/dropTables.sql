955afe2bff342c1cfb0c92fd043654b7aaccfc9a
degtyaryov <Degtyarov96@mail.ru>
2020-10-04 11:56:07 +0300

    Add drop tables sql script

---
+++ src/main/resources/database/dropTables.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS user_courses;
+DROP TABLE IF EXISTS course_runs;
+DROP TABLE IF EXISTS course_results;
+DROP TABLE IF EXISTS course_statuses;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS courses;
+DROP TABLE IF EXISTS user_roles;
