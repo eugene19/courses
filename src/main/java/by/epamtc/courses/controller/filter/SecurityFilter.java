c1b212071300103e8829d15e75d6b041f1620698
degtyaryov <Degtyarov96@mail.ru>
2020-10-04 20:58:09 +0300

    Add security filter

---
+++ src/main/java/by/epamtc/courses/controller/filter/SecurityFilter.java
@@ -0,0 +1,42 @@
+package by.epamtc.courses.controller.filter;
+
+import by.epamtc.courses.controller.command.CommandName;
+import by.epamtc.courses.entity.ParameterName;
+import by.epamtc.courses.service.PageName;
+import org.apache.log4j.Logger;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SecurityFilter implements Filter {
+    private static final Logger LOGGER = Logger.getLogger(SecurityFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+        String command = req.getParameter(ParameterName.COMMAND);
+        Object user = req.getSession(true).getAttribute(ParameterName.USER);
+
+        if (command != null
+                && command.equalsIgnoreCase(CommandName.GET_PROFILE_PAGE.toString())
+                && user == null) {
+            LOGGER.warn("Try opening private page not authored user");
+            req.getRequestDispatcher(PageName.LOGIN_PAGE).forward(req, resp);
+            return;
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
