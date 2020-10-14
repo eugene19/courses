2a79f72e25c288e0dd7358f9205f744c10c17fe7
degtyaryov <Degtyarov96@mail.ru>
2020-10-14 18:41:03 +0300

    Add removing one table in script

--- src/main/resources/database/dropTables.sql
+++ src/main/resources/database/dropTables.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS user_courses;
+DROP TABLE IF EXISTS user_course_statuses;
 DROP TABLE IF EXISTS course_runs;
 DROP TABLE IF EXISTS course_results;
 DROP TABLE IF EXISTS course_statuses;
